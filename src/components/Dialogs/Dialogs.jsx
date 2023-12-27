456a17577f0cc68d6e8033e04d396d2302842de9
Nikolay Kharitonoff <nikolay.kharitonoff@gmail.com>
2023-12-27 16:41:57 +0200

    create componetns array by method map()

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,47 +5,30 @@ import Message from "./Message/Message";
 
 const Dialogs = (props) => {
 
-    let dialogsData = [
+    let dialogs = [
         {id: 1, name: 'Dima'},
         {id: 2, name: 'Sasha'},
         {id: 3, name: 'Masha'},
         {id: 4, name: 'Dasha'},
         {id: 5, name: 'Vasya'},
-        {id: 6, name: 'Natasha'}
-    ];
+        {id: 6, name: 'Natasha'}];
 
-    let messageData = [
-        {
-            id: 0,
-            timeStamp: "26.12.2023 10:13:42",
-            message: "Hi! How are you?"
-        },
-        {
-            id: 1,
-            timeStamp: "26.12.2023 10:15:02",
-            message: "Hello! I'm fine!"
-        },
-        {
-            id: 2,
-            timeStamp: "26.12.2023 10:20:23",
-            message: "Where are you from?"
-        }
-    ];
+    let messages = [
+        {id: 0, message: "Hi! How are you?", timeStamp: "26.12.2023 10:13:42"},
+        {id: 1, message: "Hello! I'm fine!", timeStamp: "26.12.2023 10:15:02"},
+        {id: 2, message: "Where are you from?", timeStamp: "26.12.2023 10:20:23"}];
+
+    let dialogElements = dialogs.map( d => <Dialog name={d.name} userId={d.id} /> );
+
+    let messageElements = messages.map(m => <Message message={m.message} time={m.timeStamp} />);
 
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogItems}>
-                <Dialog name={dialogsData[0].name} userId={dialogsData[0].id}/>
-                <Dialog name={dialogsData[1].name} userId={dialogsData[1].id}/>
-                <Dialog name={dialogsData[2].name} userId={dialogsData[2].id}/>
-                <Dialog name={dialogsData[3].name} userId={dialogsData[3].id}/>
-                <Dialog name={dialogsData[4].name} userId={dialogsData[4].id}/>
-                <Dialog name={dialogsData[5].name} userId={dialogsData[5].id}/>
+                {dialogElements}
             </div>
             <div className={classes.messages}>
-                <Message message={messageData[0].message} time={messageData[0].timeStamp}/>
-                <Message message={messageData[1].message} time={messageData[1].timeStamp}/>
-                <Message message={messageData[2].message} time={messageData[2].timeStamp}/>
+                {messageElements}
             </div>
         </div>
     );
--- src/components/Profile/MyPost/MyPosts.jsx
+++ src/components/Profile/MyPost/MyPosts.jsx
@@ -4,18 +4,14 @@ import Post from "./Post/Post";
 
 const MyPosts = (props) => {
 
-    let postData = [
-        {
-            id: 0,
-            message: "It is my first post.",
-            likesCount: 2
-        },
-        {
-            id: 1,
-            message: "It's my second post.",
-            likesCount: 25
-        }
+    let posts = [
+        {id: 0, message: "It's my first post.", likesCount: 2},
+        {id: 1, message: "It's my second post.", likesCount: 25},
+        {id: 1, message: "It's my third post.", likesCount: 100},
+        {id: 1, message: "It's my four post.", likesCount: 13},
     ];
+
+    let postElements = posts.map(p => <Post message={p.message} likesCount={p.likesCount} />);
 
     return (
         <div>
@@ -31,8 +27,7 @@ const MyPosts = (props) => {
                 </div>
             </div>
             <div className={classes.posts}>
-                <Post message={postData[0].message} likesCount={postData[0].likesCount}/>
-                <Post message={postData[1].message} likesCount={postData[1].likesCount}/>
+                {postElements}
             </div>
         </div>
     );
