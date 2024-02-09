ae71a38452879c643de572e21d3a4c59b76725d7
Nikolay Kharitonoff <nikolay.kharitonoff@gmail.com>
2024-02-09 15:55:58 +0200

    Lesson 51 - Users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,46 +1,34 @@
 import React from 'react';
 import classes from "./Users.module.css";
+import axios from "axios";
+import userPhoto from "../../assets/images/user.png";
 
 let Users = (props) => {
 
-    if(props.users.length === 0) {
-        props.setUsers([
-            {
-                id: 0,
-                followed: false,
-                fullName: 'Dmitry',
-                status: 'I am a boss',
-                location: {city: 'Minsk', country: 'Belarus'}
-            },
-            {
-                id: 1,
-                followed: true,
-                fullName: 'Sasha',
-                status: 'I am a boss too',
-                location: {city: 'Moscow', country: 'Russia'}
-            },
-            {
-                id: 2,
-                followed: false,
-                fullName: 'Andrew',
-                status: 'I am a boss too',
-                location: {city: 'Kiev', country: 'Ukraine'}
-            }
-        ])
+    if (props.users.length === 0) {
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items)
+        })
     }
 
     return <div>
         {
             props.users.map(u => <div className={classes.user} key={u.id}>
-                <div><img className={classes.userPhoto} src='https://cdn.icon-icons.com/icons2/1812/PNG/512/4213460-account-avatar-head-person-profile-user_115386.png' /></div>
-                <div>{u.fullName}</div>
+                <div><img className={ classes.userPhoto }
+                          src={ u.photos.small != null ? u.photos.small : userPhoto } />
+                </div>
+                <div>{u.name}</div>
                 <div>{u.status}</div>
-                <div>{u.location.country}</div>
-                <div>{u.location.city}</div>
+{/*                <div>{u.location.country}</div>
+                <div>{u.location.city}</div>*/}
                 <div>{
                     u.followed
-                        ? <button onClick={ () => { props.onUnfollow(u.id) } }>Unfollow</button>
-                        : <button onClick={ () => { props.onFollow(u.id) } }>Follow</button>
+                        ? <button onClick={() => {
+                            props.onUnfollow(u.id)
+                        }}>Unfollow</button>
+                        : <button onClick={() => {
+                            props.onFollow(u.id)
+                        }}>Follow</button>
                 }</div>
             </div>)
         }
--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -2,7 +2,31 @@ const SET_USERS = "SET-USERS";
 const FOLLOW = "FOLLOW";
 const UNFOLLOW = "UNFOLLOW";
 
-let initialState = {users: []}
+let initialState = { users: []
+    /*users: [
+        {
+            id: 0,
+            followed: false,
+            fullName: 'Dmitry',
+            status: 'I am a boss',
+            location: {city: 'Minsk', country: 'Belarus'}
+        },
+        {
+            id: 1,
+            followed: true,
+            fullName: 'Sasha',
+            status: 'I am a boss too',
+            location: {city: 'Moscow', country: 'Russia'}
+        },
+        {
+            id: 2,
+            followed: false,
+            fullName: 'Andrew',
+            status: 'I am a boss too',
+            location: {city: 'Kiev', country: 'Ukraine'}
+        }
+    ]*/
+}
 
 const usersReducer = (state = initialState, action) => {
     switch (action.type) {
