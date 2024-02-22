2da9500f14194501a0573f1b74c7cab0abe6959d
Nikolay Kharitonoff <nikolay.kharitonoff@gmail.com>
2024-02-22 10:01:44 +0200

    Lesson 57 - Preloader

--- src/redux/users-reducer.js
+++ src/redux/users-reducer.js
@@ -3,13 +3,15 @@ const SET_CURRENT_PAGE = "SET-CURRENT-PAGE";
 const SET_TOTAL_USERS_COUNT = "SET-TOTAL-USERS-COUNT";
 const FOLLOW = "FOLLOW";
 const UNFOLLOW = "UNFOLLOW";
+const TOGGLE_IS_FETCHING = "TOGGLE_IS_FETCHING";
 
 
 let initialState = {
     users: [],
     pageSize: 5,
     totalUsersCount: 0,
-    currentPage: 1
+    currentPage: 1,
+    isFetching: false
 }
 
 const usersReducer = (state = initialState, action) => {
@@ -19,12 +21,12 @@ const usersReducer = (state = initialState, action) => {
                 ...state,
                 users: action.users
             }
-            case SET_CURRENT_PAGE:
+        case SET_CURRENT_PAGE:
             return {
                 ...state,
                 currentPage: action.currentPage
             }
-            case SET_TOTAL_USERS_COUNT:
+        case SET_TOTAL_USERS_COUNT:
             return {
                 ...state,
                 totalUsersCount: action.count
@@ -49,15 +51,21 @@ const usersReducer = (state = initialState, action) => {
                     return u;
                 })
             }
+        case TOGGLE_IS_FETCHING:
+            return {
+                ...state,
+                isFetching: action.isFetching
+            }
         default:
             return state;
     }
 }
 
 export const setUsersAC = (users) => ({type: SET_USERS, users})
-export const setCurrentPageAC = (currentPage) => ({type:SET_CURRENT_PAGE, currentPage})
-export const setTotalUsersCountAC = (totalUsersCount) =>({type:SET_TOTAL_USERS_COUNT, count: totalUsersCount})
+export const setCurrentPageAC = (currentPage) => ({type: SET_CURRENT_PAGE, currentPage})
+export const setTotalUsersCountAC = (totalUsersCount) => ({type: SET_TOTAL_USERS_COUNT, count: totalUsersCount})
 export const followAC = (userID) => ({type: FOLLOW, userID})
 export const unfollowAC = (userID) => ({type: UNFOLLOW, userID})
+export const toggleIsFetchingAC = (isFetching) => ({type: TOGGLE_IS_FETCHING, isFetching})
 
 export default usersReducer;
