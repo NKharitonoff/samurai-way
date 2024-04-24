a55511dd49e710709174f1453c5f44b5fc29eb2e
Nikolay Kharitonoff <nikolay.kharitonoff@gmail.com>
2024-04-24 16:28:27 +0300

    Lesson 62 - follow-unfollow API

--- src/components/Header/HeaderContainer.jsx
+++ src/components/Header/HeaderContainer.jsx
@@ -20,6 +20,7 @@ class HeaderContainer extends React.Component {
         return <Header {...this.props} />
     }
 }
+
 const mapStateToProps = (state) => ({
     isAuth: state.auth.isAuth,
     login: state.auth.login,
